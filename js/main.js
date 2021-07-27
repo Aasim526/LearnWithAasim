929c52ecd6231ea4679042a7bc9a3721a81795b3
Aasim526 <79435950+Aasim526@users.noreply.github.com>
2021-07-27 19:05:27 +0530

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// Basic, Web Dev, Hardware Toggle 
+function webDevToggle(){var basicActiveBTN=document.getElementById('basicBTN')
+var webDevActiveBTN=document.getElementById('webDevBTN')
+var hardwareActiveBTN=document.getElementById('hardwareBTN')
+var basics=document.getElementById('basics');var webDev=document.getElementById('webDev');var hardware=document.getElementById('hardware');basics.classList.add('d-none')
+hardware.classList.add('d-none')
+webDev.classList.remove('d-none')
+webDevActiveBTN.classList.add('activeBTN')
+basicActiveBTN.classList.remove('activeBTN')
+hardwareActiveBTN.classList.remove('activeBTN')}
+function hardwareToggle(){var basicActiveBTN=document.getElementById('basicBTN')
+var webDevActiveBTN=document.getElementById('webDevBTN')
+var hardwareActiveBTN=document.getElementById('hardwareBTN')
+var basics=document.getElementById('basics');var webDev=document.getElementById('webDev');var hardware=document.getElementById('hardware');basics.classList.add('d-none')
+webDev.classList.add('d-none')
+hardware.classList.remove('d-none')
+webDevActiveBTN.classList.remove('activeBTN')
+basicActiveBTN.classList.remove('activeBTN')
+hardwareActiveBTN.classList.add('activeBTN')}
+function basicsToggle(){var basicActiveBTN=document.getElementById('basicBTN')
+var webDevActiveBTN=document.getElementById('webDevBTN')
+var hardwareActiveBTN=document.getElementById('hardwareBTN')
+var basics=document.getElementById('basics');var webDev=document.getElementById('webDev');var hardware=document.getElementById('hardware');webDev.classList.add('d-none')
+hardware.classList.add('d-none')
+basics.classList.remove('d-none')
+webDevActiveBTN.classList.remove('activeBTN')
+basicActiveBTN.classList.add('activeBTN')
+hardwareActiveBTN.classList.remove('activeBTN')}
+
+
+// Menu bar toggle 
+const menu = document.getElementById ('menu');
+const navUL = document.getElementById ('navUL');
+menu.onclick = function(){
+    menu.classList.toggle('toggle');
+    navUL.classList.toggle('navUL');
+}
+
+// alert 
+alert('This is not the final version. This website is under construction.');
